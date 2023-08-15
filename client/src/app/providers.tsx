60e823272f4ad0672fe7db26bb2e6ef0cbb79c1e
Niraj Chaurasiya <developer.nirajchaurasiya@gmail.com>
2023-08-15 13:25:42 +0545

    updates

--- client/src/app/providers.tsx
+++ client/src/app/providers.tsx
@@ -5,7 +5,7 @@ import { Next13ProgressBar } from "next13-progressbar";
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <div id="background_blur">
-      <Suspense>{children}</Suspense>
+      {children}
       <Next13ProgressBar
         height="3px"
         color="var(--loading-bar-bg)"
